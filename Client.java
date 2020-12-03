d405640b363bfd30b4793558e8eb38e0ee2e104d
mcoco10 <litlemed971@gmail.com>
2020-12-03 15:15:13 +0100

    Etape 3

--- Client.java
+++ Client.java
@@ -1,25 +1,13 @@
 import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
 
 public class Client {
 
-	
-	
+	static boolean n = true;
 	private static String serverhost = "localhost";
 	private static int PORT = 51919;
-	//Ports disponibles 49152 jusqu'à 65535
-	
 	public static int i ;
-	public static int n ;
-	
-	static Scanner sct = new Scanner(System.in);
-	static Object strg ;
-	
+	//Ports disponibles 49152 jusqu'à 65535
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -28,11 +16,7 @@ public class Client {
 		 try {
 			 
 			 
-			
-			 ArrayList<Object> La = new ArrayList<Object>() ;
-			 ArrayList<Object> Attrs = new ArrayList<Object>() ;
-			 
-			 
+				
 			     System.out.println("Connexion...");
 			     s=new Socket(serverhost,PORT); 
 			     // Création du socket
@@ -45,72 +29,19 @@ public class Client {
 			     InputStream in = s.getInputStream();
 			     ObjectInputStream objIn = new ObjectInputStream(in);
 			     
-			     
-			     
-			     
+			     Integer O= new Integer(5) ;
+			     objOut.writeObject(O);
+			     System.out.println("Paquet envoyé (client"+i+") :"+O);
 			     
 			     try {
-					//Integer I= (Integer)objIn.readObject();
-			    	strg= (Object)objIn.readObject();
-					System.out.println("Paquet reçu (client"+i+""+ ") :"+strg);
-					
-					Class strgclass = strg.getClass();
-					Field[] attrs = strgclass.getFields();
-					
-					
-					
-					La.add("Objet : "+strg);
-				    La.add("ObjetClass : "+strgclass);
-					for(int i = 0; i < attrs.length; i++)
-					{
-					    Field f = attrs[i];
-					    String ClassName = f.getName() ;
-					    Class SuperClass = f.getClass().getSuperclass();
-					    Class Type=f.getType();
-					 
-					  
-					    Attrs.add("["+i+"]"+f);
-						Attrs.add("ClassNameA["+i+"] : "+ClassName);
-						Attrs.add("SuperClassA["+i+"] : "+SuperClass);
-						Attrs.add("TypeA["+i+"] : "+Type);
-					    n=i;
-					}
-					La.add("NBRattributs = "+n) ;
-					La.add("Attributs : "+Attrs);
-						
-					/*
-					Method[] m = ((Class<? extends Object>) strg).getMethods();
-	                
-					  System.out.println("Il y a " + m.length + " méthodes dans cette classe");
-					  //On parcourt le tableau de méthodes
-					  for(int i = 0; i < m.length; i++)
-					    System.out.println(m[i]);
-					*/
-					
-					
+					Integer I= (Integer)objIn.readObject();
+					System.out.println("Paquet reçu (client"+i+""
+							+ ") :"+I);
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			    
-			     /*
-			     System.out.println("Saisissez les attribut de la liste d'objet :");
-				 strg = sct.nextLine();
-				 for(int x=0;x<str.length();x++){
-				     C[x] = str.charAt(x);
-				 }
-				 System.out.println("Vous avez saisi  : " + strg);
-				 */
-			     
-			     
-				
-			     
-			     objOut.writeObject(La);
-			     System.out.println("Paquet envoyé (client"+i+") :"+La);
-			     
 			     i++;
-			     
-			     
 				
 		        s.close();
 				
--- Serveur.java
+++ Serveur.java
@@ -3,57 +3,30 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.Random;
 
 
 public class Serveur  {
     
 	public static  int i = 0 ;
-	static int tableauEntier[] = {0,1,2,3,4,5,6,7,8,9};
-	static double tableauDouble[] = {0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0};
-	static char tableauCaractere[] = {'a','b','c','d','e','f','g'};
-	static String tableauChaine[] = {"chaine1", "chaine2", "chaine3" , "chaine4"};
-	
-	static Random rand = new Random() ;
-	static int choix1 = tableauEntier[ rand.nextInt(tableauEntier.length)] ;
-	static double choix2 = tableauDouble[ rand.nextInt(tableauDouble.length)] ;
-	static char choix3 = tableauCaractere[ rand.nextInt(tableauCaractere.length)] ;
-	static String choix4 = tableauChaine[ rand.nextInt(tableauChaine.length)] ;
-	
-	
-	
+	public static int j=0;
 	public static ArrayList<MonThread> Ps = new ArrayList<MonThread>() ;
-	
-	//Liste d'Objet Quelconque
 	public static LinkedList<Object> L = new LinkedList<Object>();
-	
-	//Liste des  attributs des Objets
 	public static LinkedList<Object> Ls = new LinkedList<Object>();
-	
 	private static ServerSocket ecoute;
 	private static int PORT = 51919;
 	//Ports disponibles 49152 jusqu'à 65535
 	
 	
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
-		System.out.println("choix1 : "+choix1);
-		System.out.println("choix2 : "+choix2);
-		System.out.println("choix3 : "+choix3);
-		System.out.println("choix4 : "+choix4);
-		*/
+		
+		for(j=0;j<5;j++)
+		{
+			L.add(j);
+		}
 		
 		try {
-			
-			L.add(choix1);
-			L.add(choix2);
-			L.add(choix3);
-			L.add(choix4);
-			System.out.println("Liste : "+ L);
-			
 			// On écoute sur le port <PORT>
 			 ecoute=new ServerSocket(PORT);
 			 System.out.println("Serveur initialisé");
@@ -65,15 +38,34 @@ public class Serveur  {
 			System.out.println("Client connecté(Serveur) : "+client.getInetAddress().toString());
 			
 			
-			
-			
-			
 			MonThread  P = new MonThread("Thread"+i,client) ;
 			P.start();
 			Ps.add(P);
 			i++;
 			
+			/*
 			
+			
+			OutputStream out = client.getOutputStream();
+			ObjectOutputStream objOut = new ObjectOutputStream(out);
+			
+			
+			InputStream in = client.getInputStream();
+			ObjectInputStream objIn = new ObjectInputStream(in);
+			
+			Integer I= new Integer(3);
+			objOut.writeObject(I);
+			
+			try {
+				Integer O = (Integer)objIn.readObject();
+				System.out.println("Paquet reçu (serveur) :"+O);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			client.close();*/
 			
 			}
 		}
