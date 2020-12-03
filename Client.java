7970cd1a3bd85ee344cd1daf1309342a8e408bce
mcoco10 <litlemed971@gmail.com>
2020-12-03 13:54:51 +0100

    Etape 1

---
+++ Client.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+	private static String serverhost = "localhost";
+	private static int PORT = 51919;
+	
+	//Ports disponibles 49152 jusqu'à 65535
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Socket s=null;
+		 try {
+			     System.out.println("Connexion...");
+			     s=new Socket(serverhost,PORT); 
+			     // Création du socket
+			     System.out.println("Connecté.");
+		         // Récupération des flux d’entrée/sortie
+			     
+			     OutputStream out = s.getOutputStream();
+			     ObjectOutputStream objOut = new ObjectOutputStream(out);
+			     
+			     InputStream in = s.getInputStream();
+			     ObjectInputStream objIn = new ObjectInputStream(in);
+			     
+			     Integer O= new Integer(5) ;
+			     objOut.writeObject(O);
+			     
+			     try {
+					Integer I= (Integer)objIn.readObject();
+					System.out.println("Paquet reçu (client) :"+I);
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			     
+		         s.close();
+		} 
+		 catch (IOException e) {System.err.println(e);}
+
+
+	}
+
+}
+
---
+++ Serveur.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.*;
+
+
+public class Serveur  {
+
+	private static ServerSocket ecoute;
+	private static int PORT = 51919;
+	//Ports disponibles 49152 jusqu'à 65535
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			// On écoute sur le port <PORT>
+			 ecoute=new ServerSocket(PORT);
+			 System.out.println("Serveur initialisé");
+			 
+			while (true) {
+			// On accepte une demande de connexion d'un client
+			System.out.println("En attente de connexion sur le port "+PORT);
+			Socket client=ecoute.accept();
+			System.out.println("Client connecté : "+client.getInetAddress().toString());
+			
+			OutputStream out = client.getOutputStream();
+			ObjectOutputStream objOut = new ObjectOutputStream(out);
+			
+			
+			InputStream in = client.getInputStream();
+			ObjectInputStream objIn = new ObjectInputStream(in);
+			
+			Integer I= new Integer(3);
+			objOut.writeObject(I);
+			
+			try {
+				Integer O = (Integer)objIn.readObject();
+				System.out.println("Paquet reçu (serveur) :"+O);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			client.close();
+			
+			}
+		}
+		catch (IOException e) {
+		 System.err.println(e.getMessage());
+		 System.exit(1);
+		 }
+
+
+	}
+
+}
+
